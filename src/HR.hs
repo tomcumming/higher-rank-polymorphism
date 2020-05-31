4b749a6f277f25524e0a5db7c0074fdcd1e18498
Tom Cumming <mail@tomcumming.co.uk>
2020-05-31 19:58:19 +0100

    Show simplified expressions

--- src/HR.hs
+++ src/HR.hs
@@ -3,6 +3,7 @@ module HR where
 import qualified Data.Map as Map
 import qualified Simple.Check as Check
 import qualified Simple.Expr.Source as Source
+import qualified Simple.Expr.Typed as Typed
 import Simple.TI (runTI)
 import qualified Simple.Type as Type
 import qualified Simple.Unify as Unify
@@ -12,7 +13,14 @@ ctx =
   Map.fromList
     [ ("id", Type.Forall "a" $ Type.Arrow (Type.Var "a") (Type.Var "a")),
       ("f", Type.Arrow (Type.Arrow Type.Unit Type.Unit) Type.Unit),
-      ("g", Type.Arrow (Type.Forall "b" $ Type.Arrow (Type.Var "b") (Type.Var "b")) Type.Unit)
+      ("g", Type.Arrow (Type.Forall "b" $ Type.Arrow (Type.Var "b") (Type.Var "b")) Type.Unit),
+      -- forall b. (forall a. a -> b) -> b
+      ( "escape",
+        Type.Forall "b" $
+          Type.Arrow
+            (Type.Forall "a" $ Type.Arrow (Type.Var "a") (Type.Var "b"))
+            (Type.Var "b")
+      )
     ]
 
 testE :: Source.Expr
@@ -24,3 +32,14 @@ testE2 = Source.App (Source.Var "f") (Source.Var "id")
 testE3 = Source.App (Source.Var "g") (Source.Abs "x" $ Source.Var "x")
 
 testE4 = Source.App (Source.Var "f") (Source.Abs "x" $ Source.Var "x")
+
+testEscape = Source.App (Source.Var "escape") (Source.Abs "x" $ Source.Var "x")
+
+checkExpr :: Source.Expr -> IO ()
+checkExpr e = do
+  let (s, t, ce) = runTI $ Check.infer ctx e
+  print (s, t, ce)
+  let t2 = Unify.subs s t
+  let ce2 = Typed.simplify $ Typed.subs s ce
+  print t2
+  print ce2
--- src/Simple/Expr/Typed.hs
+++ src/Simple/Expr/Typed.hs
@@ -2,6 +2,7 @@ module Simple.Expr.Typed where
 
 import Simple.Expr (Id)
 import qualified Simple.Type as Type
+import qualified Simple.Unify as Unify
 
 data Expr
   = Unit
@@ -11,3 +12,21 @@ data Expr
   | TAbs Type.Id Expr
   | TApp Expr Type.Type
   deriving (Show, Eq)
+
+subs :: Unify.Subs -> Expr -> Expr
+subs s e = case e of
+  Abs x t e -> Abs x (Unify.subs s t) (subs s e)
+  App e1 e2 -> App (subs s e1) (subs s e2)
+  TAbs x e -> TAbs x (subs s e)
+  TApp e t -> TApp (subs s e) (Unify.subs s t)
+  e -> e
+
+-- deep eta reduction
+simplify :: Expr -> Expr
+simplify e = case e of
+  TAbs x (TApp e (Type.Var y)) | x == y -> simplify e
+  Abs x t e -> Abs x t (simplify e)
+  App e1 e2 -> App (simplify e1) (simplify e2)
+  TAbs x e -> TAbs x (simplify e)
+  TApp e t -> TApp (simplify e) t
+  e -> e
